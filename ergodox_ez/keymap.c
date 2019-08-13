05f31ba093a1b1f8092649b29e6f5adcfc7925be
Christian Brevik <christian.brevik@gmail.com>
2019-08-13 10:55:42 +0200

    Reset dance tap limit to 150
    Longer duration didn't work well with arrow keys

--- ergodox_ez/keymap.c
+++ ergodox_ez/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTRL, KC_LGUI, KC_LALT, KC_RALT, KC_RCTRL,
 
     // mid left
-    TG(DEV), TG(SYMBOLS), TG(MOUSE), KC_SPACE, KC_SPACE, KC_END,
+    TG(DEV), TG(SYMBOLS), TG(OVERWATCH), KC_SPACE, KC_SPACE, KC_END,
     
     // right
     NO_ACUT, KC_6, KC_7, KC_8, KC_9, KC_0, NO_PLUS,
