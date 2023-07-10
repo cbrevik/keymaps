eedad6002c5e177703559e5ab84da3ea0283ce01
Christian Brevik <christian.brevik@gmail.com>
2023-07-10 21:59:24 +0200

    more includes + fixes

--- moonlander/keymap.c
+++ moonlander/keymap.c
@@ -1,17 +1,24 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "keymap_nordic.h"
 #include "keymap_norwegian.h"
+#include "keymap_contributions.h"
 
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define NO_TH ALGR(KC_T)
-#define NO_ETH ALGR(KC_D)
+// Norwegian unique MAC characters
+#define NO_ACUT_MAC KC_EQL           // =
+#define NO_APOS_MAC KC_NUBS          // '
+#define NO_AT_MAC KC_BSLS            // @
+#define NO_BSLS_MAC ALGR(LSFT(KC_7)) // '\'
+#define NO_DLR_MAC LSFT(KC_4)        // $
+#define NO_GRV_MAC ALGR(NO_BSLS)     // `
+#define NO_GRTR_MAC LSFT(KC_GRV)     // >
+#define NO_LCBR_MAC ALGR(LSFT(KC_8)) // }
+#define NO_LESS_MAC KC_GRV           // >
+#define NO_PIPE_MAC ALGR(KC_7)       // |
+#define NO_RCBR_MAC ALGR(LSFT(KC_9)) // }
+
+#define NO_DIAE KC_RBRC // ¨ (dead)
+#define NO_AA KC_LBRC
 
 enum custom_keycodes
 {
@@ -25,7 +32,7 @@ enum custom_keycodes
   VIM_RIGHT_10
 };
 
-//Tap Dance Declarations
+// Tap Dance Declarations
 enum
 {
   TD_GRTR_GRTRCLOSE = 0,
@@ -51,7 +58,7 @@ void grtr_tapdance(qk_tap_dance_state_t *state, void *user_data)
   unregister_code(KC_LSFT);
 }
 
-//Tap Dance Definitions
+// Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_GRTR_GRTRCLOSE] = ACTION_TAP_DANCE_FN(grtr_tapdance),
 };
@@ -61,24 +68,24 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer: BASE MAC
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |    '   |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   +    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  {   |           |   }  |   Y  |   U  |   I  |   O  |   P  |   Å    |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |   Esc  |   A  |   S  |   D  |   F  |   G  |  (   |           |   )  |   H  |   J  |   K  |   L  |   Ø  |   Æ    |
- * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  |   -  | RShift |
- * `--------+------+------+------+------+------'                         `------+------+------+------+------+--------'
- *   | LCtrl| Cmd  | LAlt | RAlt |~META |  ,-------------.     ,--------------. | LEFT | DOWN |  UP  | RIGHT| ~META  |
- *   `----------------------------------'  |      <      |     |  >/=>//>     | `----------------------------------'                                                                                       
- *                                  ,------|------|------|     |------+-------+------.
- *                                  |      |      |      |     |      |       |      |
- *                                  | Space| Space|Delete|     |BckSpc| Enter |Enter |
- *                                  |      |      |      |     |      |       |      |
- *                                  `--------------------'     `---------------------'
- */
+     *
+     * ,--------------------------------------------------.           ,--------------------------------------------------.
+     * |    '   |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   +    |
+     * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+     * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  {   |           |   }  |   Y  |   U  |   I  |   O  |   P  |   Å    |
+     * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
+     * |   Esc  |   A  |   S  |   D  |   F  |   G  |  (   |           |   )  |   H  |   J  |   K  |   L  |   Ø  |   Æ    |
+     * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
+     * | LShift |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  |   -  | RShift |
+     * `--------+------+------+------+------+------'                         `------+------+------+------+------+--------'
+     *   | LCtrl| Cmd  | LAlt | RAlt |~META |  ,-------------.     ,--------------. | LEFT | DOWN |  UP  | RIGHT| ~META  |
+     *   `----------------------------------'  |      <      |     |  >/=>//>     | `----------------------------------'
+     *                                  ,------|------|------|     |------+-------+------.
+     *                                  |      |      |      |     |      |       |      |
+     *                                  | Space| Space|Delete|     |BckSpc| Enter |Enter |
+     *                                  |      |      |      |     |      |       |      |
+     *                                  `--------------------'     `---------------------'
+     */
     [BASE_MAC] = LAYOUT_moonlander(
         KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, NO_LBRC, NO_RBRC, KC_6, KC_7, KC_8, KC_9, KC_0, NO_PLUS,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, NO_LCBR_MAC, NO_RCBR_MAC, KC_Y, KC_U, KC_I, KC_O, KC_P, NO_AA,
@@ -87,24 +94,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL, KC_LGUI, KC_LALT, KC_RALT, MO(META), NO_LESS, TD(TD_GRTR_GRTRCLOSE), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, MO(META),
         KC_SPACE, KC_SPACE, KC_DEL, KC_BSPACE, KC_ENTER, KC_ENTER),
     /* Layer: Dev meta
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |   ´  |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |  ¨   |           |   @  |      |      |      |      |      |   F12  |
- * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |        |      |      |      |      |      |  ^   |           |   ~  |LEFT10| UP 10|DOWN10|RGHT10|   ;  |    =   |
- * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                         |      |      |      |      |      |        |
- * `--------+------+------+------+------+------'                         `------+------+------+------+------+--------'
- *   |      |      |      |      |      |  ,-------------.     ,--------------. | HOME |PGDOWN| PGUP |  END |        |
- *   `----------------------------------'  |             |     |              | `----------------------------------'                                                                                       
- *                                  ,------|------|------|     |------+-------+------.
- *                                  |      |      |      |     |      |       |      |
- *                                  |      |      |      |     |      |       |      |
- *                                  |      |      |      |     |      |       |      |
- *                                  `--------------------'     `---------------------'
- */
+     *
+     * ,--------------------------------------------------.           ,--------------------------------------------------.
+     * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |   ´  |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+     * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+     * |        |      |      |      |      |      |  ¨   |           |   @  |      |      |      |      |      |   F12  |
+     * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
+     * |        |      |      |      |      |      |  ^   |           |   ~  |LEFT10| UP 10|DOWN10|RGHT10|   ;  |    =   |
+     * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
+     * |        |      |      |      |      |      |                         |      |      |      |      |      |        |
+     * `--------+------+------+------+------+------'                         `------+------+------+------+------+--------'
+     *   |      |      |      |      |      |  ,-------------.     ,--------------. | HOME |PGDOWN| PGUP |  END |        |
+     *   `----------------------------------'  |             |     |              | `----------------------------------'
+     *                                  ,------|------|------|     |------+-------+------.
+     *                                  |      |      |      |     |      |       |      |
+     *                                  |      |      |      |     |      |       |      |
+     *                                  |      |      |      |     |      |       |      |
+     *                                  `--------------------'     `---------------------'
+     */
 
     [META] = LAYOUT_moonlander(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, NO_ACUT_MAC, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
@@ -115,7 +122,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_VAD, RGB_VAI, _______, RGB_SLD, RGB_HUD, RGB_HUI),
 };
 
-extern bool g_suspend_state;
 extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void)
@@ -155,7 +161,11 @@ void set_layer_color(int layer)
 
 void rgb_matrix_indicators_user(void)
 {
-  if (g_suspend_state || keyboard_config.disable_layer_led)
+  if (rawhid_state.rgb_control)
+  {
+    return;
+  }
+  if (keyboard_config.disable_layer_led)
   {
     return;
   }
